dddc0ad76a9e1871be45ea718e1efe23e4e17a14
HolyPi <joanelly2018@gmail.com>
2019-09-03 11:00:38 -0700

    Added more user inputs on line 20

--- MadLibs.py
+++ MadLibs.py
@@ -2,6 +2,9 @@
 print ("Enter an Animal.")
 Animal = [input()]
 
+print ("Enter anything.")
+Anything = [input()]
+
 print ("Enter 2 PastVerbs.")
 PastVerbs = [input(), input()]
 
@@ -14,8 +17,9 @@ def show():
     print("when she suddenly saw a " + Animal[0],)
     print(" (What are you doing here? Did you lose your way?) ")
     print(" And it just disappeared into thin air with the most mischievous smile. ")
-    print(" Walking further into the carved path led her into an even more obscure place, so she " + PastVerbs[0],
-    "and " + PastVerbs[1] + " at full speed out of the forest. ")
+    print(" Walking further into the carved path led her into an even more obscure place, where she saw " + Anything[0],)
+    print (" So she " + PastVerbs[0],)
+    print ("and " + PastVerbs[1] + " at full speed out of the forest. ")
     print(Verb[0] + " she finally came into a less densely and obscure place. ")
     print(" She felt a ray of light against her face, and looked up. ")
     print(" She saw the hole she probably fell into. ")
